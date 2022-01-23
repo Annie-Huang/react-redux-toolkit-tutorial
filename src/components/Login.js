e8c8cfb88ae74281f300ff7314ea025f1d3d0c2c
Annie Huang <annie.hn@gmail.com>
2022-01-23 19:00:12 +1100

    In the component, dispatch the login action we just created. Hardcode the value we update and verified we can see it in the web.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../features/user';
 
 const Login = () => {
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(
+            login({
+              name: 'Pedro',
+              age: 20,
+              email: 'pedro@gmail.com',
+            })
+          );
+        }}
+      >
+        Login
+      </button>
     </div>
   );
 };
