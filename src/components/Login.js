48ed91062e8e1bcd0a0d3e1a07d10dcdff205f1f
Annie Huang <annie.hn@gmail.com>
2022-01-25 19:56:15 +1100

    dispatch the logout action in Login.js and verified that it working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { login } from '../features/user';
+import { login, logout } from '../features/user';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -20,6 +20,7 @@ const Login = () => {
       >
         Login
       </button>
+      <button onClick={() => dispatch(logout())}>LOGOUT</button>
     </div>
   );
 };
