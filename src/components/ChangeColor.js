5a4d3995bc8a08199198ccbb28d338cc9e2dd0f4
Annie Huang <annie.hn@gmail.com>
2022-01-25 20:07:26 +1100

    Create the ChangeColor component

---
+++ src/components/ChangeColor.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const ChangeColor = () => {
+  const [color, setColor] = useState('');
+
+  return (
+    <div>
+      <input type='text' onChange={(event) => setColor(event.target.value)} />
+      <button>CHANGE COLOR</button>
+    </div>
+  );
+};
+
+export default ChangeColor;
