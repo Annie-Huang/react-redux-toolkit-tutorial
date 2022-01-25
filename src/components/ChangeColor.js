d45ba466046f9f5d9b395d3a3ca3dc08ee3c7039
Annie Huang <annie.hn@gmail.com>
2022-01-25 20:19:08 +1100

    Add dispatch in ChangeColor.js and dispatch changeColor action when the button is clicked. Verified the change has change through redux dev tool

--- src/components/ChangeColor.js
+++ src/components/ChangeColor.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { changeColor } from '../features/theme';
 
 const ChangeColor = () => {
   const [color, setColor] = useState('');
+  const dispatch = useDispatch();
 
   return (
     <div>
       <input type='text' onChange={(event) => setColor(event.target.value)} />
-      <button>CHANGE COLOR</button>
+      <button onClick={() => dispatch(changeColor(color))}>CHANGE COLOR</button>
     </div>
   );
 };
